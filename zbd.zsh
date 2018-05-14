423cb386f82fda263d3793f7c673b72d584efe76
Ryuto-MacBook <26628494+ryutok@users.noreply.github.com>
2018-05-14 19:19:04 +0900

    Clean up code.

--- zbd.zsh
+++ zbd.zsh
@@ -1,15 +1,10 @@
 function zbd {
     case $1 in
-        (-h|--help)
-            __zbd:help;;
-        (--)
-            __zbd:count $2;;
-        (-*)
-            __zbd:illegal_opt $1;;
-        ("")
-            __zbd:cd 1;;
-        (*)
-            __zbd:count $1;;
+        -h | --help ) __zbd:help;;
+                 -- ) __zbd:count $2;;
+                 -* ) __zbd:illegal_opt $1;;
+                 "" ) __zbd:cd 1;;
+                  * ) __zbd:count $1;;
     esac
 }
 
@@ -32,7 +27,6 @@ function __zbd:cd {
         dest=`repeat $1 printf "../"`
         cd $dest
     fi
-    return
 }
 
 function __zbd:count {
@@ -40,8 +34,7 @@ function __zbd:count {
     __zbd:get_dirs
     if [[ ${reply[(I)$1]} -eq 0 ]]; then
         expr "$1" + 1 >/dev/null 2>&1
-        if [ $? -lt 2 ]
-        then
+        if [ $? -lt 2 ]; then
             __zbd:cd $1
         else
             __zbd:no_name_error $1
@@ -50,13 +43,10 @@ function __zbd:count {
         num=$(( ${#reply} - ${reply[(I)$1]} + 1 ))
         __zbd:cd $num
     fi
-    return
 }
 
 function __zbd:get_dirs {
-    local i
-    reply=(${(ps:/:)${PWD}})
-    reply=( "/" $reply )
+    reply=("/" ${(ps:/:)${PWD}})
     reply=($reply[1,-2])
 }
 
